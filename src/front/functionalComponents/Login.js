d02b7658a1d502d721c46cde9f490a808aaecf5e
conpantuflas <3280kg@gmail.com>
2022-11-17 01:24:19 -0300

    commit

--- src/front/functionalComponents/Login.js
+++ src/front/functionalComponents/Login.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import RenderUser from "./RenderUser";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const tokenLogin = {
-    token: "",
-    userId: 0,
-  };
+  const [token, setToken] = useState("");
 
+  // const tokenLogin = {
+  //   token: "",
+  //   userId: 0,
+  // };
+
+  const navigate = useNavigate();
   const login = useCallback((e) => {
     e.preventDefault();
     fetch("http://localhost:5000/login", {
@@ -24,18 +29,11 @@ const Login = () => {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        tokenLogin.token = data.access_token;
-        tokenLogin.userId = data.user.id;
-        console.log(data);
+        setToken(data.access_token);
+        //tokenLogin.userId = data.user.id;
+        navigate("/renderuser");
       });
   });
-
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (tokenLogin.token != "") {
-      navigate("/renderuser");
-    }
-  }, []);
 
   return (
     <div>
@@ -53,13 +51,15 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </form>
-      <button
+      <Link
+        to="/renderuser"
         onClick={(e) => {
           login(e);
+          <RenderUser />;
         }}
       >
         Login
-      </button>
+      </Link>
     </div>
   );
 };
